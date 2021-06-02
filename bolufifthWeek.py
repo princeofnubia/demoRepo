e8b8e770a9a537c77d2f3b31727501156b525123
create1666 <create1666@gmail.com>
2021-06-02 15:33:13 +0100

    fn:vowelChecker

--- bolufifthWeek.py
+++ bolufifthWeek.py
@@ -8,3 +8,10 @@ def search(numbers):
 
 print(search([1,3,4,5,6]))
 print(search([1,3,4,5,6,4,4,4,4]))
+
+
+def vowel_checker(letter):
+  vowel_variables = 'aeiou' 
+  return letter in vowel_variables
+
+print(vowel_checker('d', 'e'))
