5e6e7ce12d110fffffcded1b986cc30803ba1941
ayodeji1051 <ayodeji.dawodu@yahoo.com>
2021-06-04 12:24:27 -0400

    twinke twinkle little star

---
+++ general/exercise.py
@@ -0,0 +1,13 @@
+#  Write a Python program to print the following string in a specific format (see the outpu
+def twinkle():
+    print('Twinkle,twinkle,little,star')
+    print('        How,are,wonder,what,you,are')
+    print('                  Up,above,the,what,so,high,')
+    print('Like,a,diamond,in,the,sky')
+
+# Write a Python program which accepts the radius of a circle from the user and compute the area. 
+
+
+twinkle()
+          
+
