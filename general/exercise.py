4676ca7849b45042363d66a08c7fb6a66e00bc75
Jokotoluwa <84516477+Jokotoluwa@users.noreply.github.com>
2021-06-07 12:59:37 +0100

    2nd exercise

--- general/exercise.py
+++ general/exercise.py
@@ -15,5 +15,25 @@ def exam_st_date(date,month,year):
     print("The examination will start from:{0}/{1}/{2} ".format (date,month,year))
 
 
+#Question 15
+def volume_of_sphere():
+    pi=3.14
+    radius=input("The radius of the sphere is: ")
+    volume=4.0/3.0*pi*int(radius)**3
+    print("The volume of the sphere is: ",volume)
+#Question 17
+def near_thousand():
+    n=int(input("The number is: "))            
+    return((abs(1000-n)<=100) or (abs(2000-n)<=100))
+
+    
+#Question 18
+def sum_numbers(x,y,z):
+    sump=x+y+z
+    if x==y==z:
+        sump=sump*3
+    return sump
+
+
 
 
